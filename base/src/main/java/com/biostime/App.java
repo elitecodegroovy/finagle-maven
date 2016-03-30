cf9be68b2422a40b0b1a99c5a356ca22fd768357
JohnLau <elite_jigang@163.com>
2016-03-30 11:08:08 +0800

    init project

---
+++ base/src/main/java/com/biostime/App.java
@@ -0,0 +1,13 @@
+package com.biostime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
